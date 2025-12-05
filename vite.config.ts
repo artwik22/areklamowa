562adcd1b961c0dd99f48e583465814130cd446a
artwik22 <igorartwik@proton.me>
2025-12-05 22:03:32 +0100

    as

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: 4173,
+    strictPort: true,
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
 })
